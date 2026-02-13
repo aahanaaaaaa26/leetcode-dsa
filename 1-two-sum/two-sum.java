4bc469c879726d233d5d18f2caf9f8f1df27dc28
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-02-13 23:03:28 +0530

    Time: 2 ms (99.17%) | Memory: 47.3 MB (20.34%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,16 +3,11 @@ class Solution {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-
+        for (int i=0; i<nums.length; i++) {
             int need = target - nums[i];
-
-            // check karo required number pehle se hai ya nahi
             if (map.containsKey(need)) {return new int[] { map.get(need), i };
             }
-
-            // current number store karo
-            map.put(nums[i], i);
+          map.put(nums[i], i);
         }
 
         return new int[]{};
