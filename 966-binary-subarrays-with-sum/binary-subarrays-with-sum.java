5cd71d5434a0ce26d62a25f22695ccd911129ff9
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-02-25 12:12:46 +0530

    Time: 22 ms (41.92%) | Memory: 52.7 MB (23.10%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -4,18 +4,15 @@ class Solution {
         int n=nums.length;
         int curr_sum=0;
         int count=0;
-      
         map.put(0,1);
         for(int i=0;i<n;i++){
             curr_sum+=nums[i];
 
-        
-        if (map.containsKey(curr_sum-goal)){
+          if (map.containsKey(curr_sum-goal)){
             count+=map.get(curr_sum-goal);
         }
         map.put(curr_sum,map.getOrDefault(curr_sum,0)+1);
         }
-        return count;
-        
+        return count;  
     }
 }
