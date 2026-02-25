f7d6403217920ddccbb0d4e5ea3779caa34d831f
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-02-25 12:43:57 +0530

    Time: 3 ms (53.12%) | Memory: 51 MB (51.05%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,20 +1,46 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-        HashMap<Integer,Integer>map= new HashMap<>();
+    public int numSubarraysWithSum(int[] nums, int k) {
+        // HashMap<Integer,Integer>map= new HashMap<>();
+        // int n=nums.length;
+        // int curr_sum=0;
+        // int count=0;
+        // map.put(0,1);
+        // for(int i=0;i<n;i++){
+        //     curr_sum+=nums[i];
+
+        //   if (map.containsKey(curr_sum-goal)){
+        //     count+=map.get(curr_sum-goal);
+        // }
+        // map.put(curr_sum,map.getOrDefault(curr_sum,0)+1);
+        // }
+        // return count; 
+        
+        int i=0;
+        int j=0;
+        int count=0;
         int n=nums.length;
         int curr_sum=0;
-        int count=0;
-        map.put(0,1);
-        for(int i=0;i<n;i++){
-            curr_sum+=nums[i];
-
-          if (map.containsKey(curr_sum-goal)){
-            count+=map.get(curr_sum-goal);
+        int zero_count=0;
+        while(j<n){
+            curr_sum += nums[j];
+            while(i<j && (curr_sum>k || nums[i]==0)){
+                if(nums[i]==0){
+                    zero_count++;
+                }
+                else{
+                    zero_count=0;
+                }
+                curr_sum -= nums[i];
+                i++;
+            }
+            if(curr_sum ==k){
+                count += 1+zero_count;
+            }
+            j++;
         }
-        map.put(curr_sum,map.getOrDefault(curr_sum,0)+1);
-        }
-        return count; 
-
-            
+        return count;
     }
 }
+
+            
+    
