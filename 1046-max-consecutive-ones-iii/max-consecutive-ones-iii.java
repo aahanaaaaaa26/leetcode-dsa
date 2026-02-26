a15ece11fb7a8066c26ed5533481554a21655fcf
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-02-26 12:23:14 +0530

    Time: 3 ms (97.92%) | Memory: 52.1 MB (74.53%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,31 +1,36 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         //agar k 2 hai toh iska mtlb longest subarray mai tum maximum length of 1 ke liye max to max 2 zero ko 1 bnake longest subarray of 1 bna skte h....
-
-
-
-        //gptttt
-        int left = 0;
-        int zeros = 0;
-        int maxLen = 0;
+        int i=0;
+        int j=0;
+        int curr_sum=0;
         int n = nums.length;
-
-        for (int right = 0; right < n; right++) {
-            if (nums[right] == 0) {
-                zeros++;          // 0 mila
+        int ans=0;
+        int zero=0;
+        for(int a=0;a<n;a++){
+            if(nums[a]==0){
+                zero++;
             }
-
-            while (zeros > k) {   // zyada 0 ho gaye
-                if (nums[left] == 0) {
-                    zeros--;
-                }
-                left++;           // window chhoti karo
-            }
-
-            maxLen = Math.max(maxLen, right - left + 1);
         }
+        if (zero<k) return n;
+        while(j<n){
 
-        return maxLen;
+            curr_sum+=nums[j];
+
+            if(j-i+1 - curr_sum > k){
+                curr_sum -= nums[i];
+                i++;
+
+            }
+            else if (j-i+1-curr_sum==k){
+                ans=Math.max(ans,j-i+1);
+              
+
+            }
+              j++;
+        }
+        return ans;
+      
     }
 }
 
