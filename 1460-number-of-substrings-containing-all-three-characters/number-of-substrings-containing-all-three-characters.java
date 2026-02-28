76e7349c0803e17ccc75e082e129de83524ae5d1
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-02-28 19:25:27 +0530

    Time: 15 ms (51.30%) | Memory: 46.4 MB (44.05%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        
+        int i = 0 , j = 0 , n = s.length() ;
+        int[] ans = new int[3] ;
+        if(n < 3) return 0;
+        int count = 0 ;
+        while(j < n)
+        {
+            //putting element
+            // 0 + 1 = 1
+            ans[s.charAt(j)-'a']++ ; // ans['a'-'a']-> ans[97-97] = ans[0]
+            while(ans[0] >= 1 && ans[1] >= 1 && ans[2] >= 1)
+            {
+                // element remove
+                count += n - j ;
+                ans[s.charAt(i)-'a']-- ;
+                i++ ;
+            }
+            j++ ;
+        }
+        return count ;
+
+    }
+}
