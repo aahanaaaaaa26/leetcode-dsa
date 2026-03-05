eb901eba8fd085d6691d7fe72ba07068a6f8f66b
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-03-05 19:32:55 +0530

    Time: 35 ms (44.08%) | Memory: 59.3 MB (44.99%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,108 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums); // step 1
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>>result=new ArrayList<>(); 
+        Arrays.sort(nums);
+        int n=nums.length;
 
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-
-            // duplicate i skip
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
+        for(int i=0;i<n-2;i++){
+      if(i>0 && nums[i]==nums[i-1])continue;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if (sum==0){
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]==nums[left+1])left++;
+                    while(left<right && nums[right]==nums[right-1])right--;
                     left++;
                     right--;
-
-                    // duplicate left skip
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-
-                    // duplicate right skip
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                }
-
-                else if (sum < 0) {
-                    left++;
-                }
-
-                else {
-                    right--;
-                }
             }
-        }
-        return ans;
+                    else if(sum<0){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }}            }
+        return result;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Arrays.sort(nums); // step 1
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         int n = nums.length;
+
+//         for (int i = 0; i < n; i++) {
+
+//             // duplicate i skip
+//             if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+//             int left = i + 1;
+//             int right = n - 1;
+
+//             while (left < right) {
+//                 int sum = nums[i] + nums[left] + nums[right];
+
+//                 if (sum == 0) {
+//                     ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+//                     left++;
+//                     right--;
+
+//                     // duplicate left skip
+//                     while (left < right && nums[left] == nums[left - 1]) left++;
+
+//                     // duplicate right skip
+//                     while (left < right && nums[right] == nums[right + 1]) right--;
+//                 }
+
+//                 else if (sum < 0) {
+//                     left++;
+//                 }
+
+//                 else {
+//                     right--;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
