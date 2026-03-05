988fd25f72e7c6fab0398ebd351ccaca314af228
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-03-05 12:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (49.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,54 +3,24 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        boolean firstRow = false, firstCol = false;
+        int[] row = new int[n];
+        int[] col = new int[m];
 
-        // check first row
-        for (int j = 0; j < m; j++) {
-            if (matrix[0][j] == 0) {
-                firstRow = true;
-                break;
-            }
-        }
-
-        // check first column
+        
         for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                firstCol = true;
-                break;
-            }
-        }
-
-        // mark rows and columns
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+            for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        // set zeroes based on marks
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
-            }
-        }
-
-        // zero first row
-        if (firstRow) {
-            for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        // zero first column
-        if (firstCol) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
             }
         }
     }
