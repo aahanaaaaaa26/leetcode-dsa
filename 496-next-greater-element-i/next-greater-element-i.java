4a1fce28345259cd4586a7b58f650602931e43f0
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-03-07 12:20:42 +0530

    Time: 4 ms (73.43%) | Memory: 45.3 MB (47.01%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,22 +1,50 @@
+// 
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            ans[i]=-1;
-        }
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    // next largest find karo
-                    for(int k=j+1;k<nums2.length;k++){
-                        if(nums2[j]<nums2[k]){
-                            ans[i]=nums2[k];
-                            break;
-                        }
-                    }
+    public static void Solve(int[] nums , HashMap<Integer,Integer> map)
+    {
+        Stack<Integer> stack  = new Stack<>() ;
+        int n = nums.length ;
+        for(int i = n-1 ; i >= 0 ; i--)
+        {
+            if(stack.isEmpty())
+            {
+                map.put(nums[i],-1) ;
+            }
+            else if(!stack.isEmpty() && stack.peek() > nums[i])
+            {
+                map.put(nums[i],stack.peek()) ;
+            }
+            else if(!stack.isEmpty() && stack.peek() <= nums[i])
+            {
+                while(!stack.isEmpty() && stack.peek() <= nums[i])
+                {
+                    stack.pop() ;
+                }
+                if(!stack.isEmpty())
+                {
+                    map.put(nums[i],stack.peek()) ;
+                }
+                else
+                {
+                    map.put(nums[i],-1) ;
                 }
             }
+            stack.push(nums[i]) ;
         }
-        return ans;
+    }
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>() ;
+        int[] ans = new int[nums1.length] ;
+        for(int i = 0 ; i < nums1.length ; i++)
+        {
+            ans[i] = -1;
+        }
+        Solve(nums2,map) ;
+        for(int i = 0 ; i<nums1.length ; i++)
+        {
+            ans[i] = map.get(nums1[i]) ;
+        }
+        return ans ;
     }
 }
