e67d7ee1074b21120bd2d5ab317a85b32b6c830f
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-03-26 12:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (89.37%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,40 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low=0;
-        int n=nums.length;
-        int high = n - 1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+    // 2 pointer
+//     public int search(int[] nums, int target) {
+//         int low=0;
+//         int n=nums.length;
+//         int high = n - 1;
+//         while(low<=high){
+//             int mid = (low+high)/2;
+//             if(nums[mid]==target){
+//                 return mid;
+//             }
+//             else if(nums[mid]<target){
+//                 low=mid+1;
+//             }
+//             else{
+//                 high=mid-1;
+//             }
         
+//         }
+//          return -1;
+//     }
+// }
+
+ public int search(int[] nums, int target) {
+        return solve(nums, target, 0, nums.length - 1);
+    }
+
+    public int solve(int[] nums, int target, int low, int high) {
+        if (low > high) return -1;   
+        int mid = low+(high - low) / 2;
+        if (nums[mid] == target) {
+            return mid; }
+        else if (nums[mid] < target) {
+            return solve(nums, target, mid + 1, high);
         }
-         return -1;
+        else {
+            return solve(nums, target, low, mid - 1);
+        }
     }
 }
