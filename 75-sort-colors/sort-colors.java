1d2dc729d7d21151805ad8cb5e03384d170ab9d5
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-03-27 11:56:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (72.24%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,3 +1,36 @@
+// class Solution {
+//     public static void swap(int[] nums , int i , int j)
+//     {
+//         int temp = nums[i] ;
+//         nums[i] = nums[j] ;
+//         nums[j] = temp ;
+//     }
+//     public void sortColors(int[] nums) {
+        
+//         int n = nums.length ;
+//         int Zero = 0 , One = 0 , Two = n-1 ;
+
+//         while(One <= Two)
+//         {
+//             if(nums[One] == 0)
+//             {
+//                 swap(nums, One , Zero) ;
+//                 One++ ;
+//                 Zero++ ;
+//             }
+//             else if(nums[One] == 2)
+//             {
+//                 swap(nums,One,Two) ;
+//                 Two-- ;
+//             }
+//             else
+//             {
+//                 One++ ;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
   public void sortColors(int[] nums) {
     int zero = -1;
