9f6c0e064ac6a55cc0704560e968e61c88eaa018
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-04-10 12:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (76.65%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -9,21 +9,48 @@
  *     }
  * }
  */
+// USING HASHSET
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         HashSet<ListNode> set = new HashSet<>();
 
+//         ListNode temp = head;
+
+//         while (temp != null) {
+//             if (set.contains(temp)) {
+//                 return temp; 
+//             }
+//             set.add(temp);
+//             temp = temp.next;
+//         }
+
+//         return null; 
+//     }
+// }
+
+
+
+//USING SLOW FAST
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-
-        ListNode temp = head;
-
-        while (temp != null) {
-            if (set.contains(temp)) {
-                return temp; 
-            }
-            set.add(temp);
-            temp = temp.next;
+        if(head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+        if(slow == fast){
+            ListNode curr = head;
+        while(curr!= slow){
+            curr = curr.next;
+            slow = slow.next;
         }
-
-        return null; 
+        return curr;
+        }
+    }
+    return null;
     }
 }
+
+
