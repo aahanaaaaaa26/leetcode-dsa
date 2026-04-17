8535598f845b10902ae1e905683944f68c7a81eb
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-04-17 21:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (69.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,57 +1,102 @@
+// class Solution {
+
+//     //rotation kaha hui check
+
+//     public static int FindMin(int low , int high , int[] nums)
+//     {
+//         while(low < high)
+//         {
+//             int mid = low + (high-low)/2 ;
+//             if(nums[mid] > nums[high])
+//             {
+//                 low = mid + 1 ;
+//             }
+//             else
+//             {
+//                 high = mid ;
+//             }
+//         }
+
+//         return low ;
+//     }
+//     //target kaha hai check
+
+//     public static int BinarySearch(int low , int high , int[] nums , int target)
+//     {
+//         while(low <= high)
+//         {
+//             int mid = low + (high-low)/2 ;
+
+//             if(nums[mid] == target)
+//             {
+//                 return mid ;
+//             }
+//             else if(nums[mid] < target)
+//             {
+//                 low = mid + 1 ;
+//             }
+//             else
+//             {
+//                 high = mid-1 ;
+//             }
+//         }
+//         return -1 ;
+//     }
+//     //dono k agar mila toh do vrna -1
+//     public int search(int[] nums, int target) {
+        
+//         int n = nums.length ;
+
+//         int index = FindMin(0,n-1,nums) ;
+//         int ans = BinarySearch(0,index-1,nums,target) ;
+
+//         if(ans != -1)
+//         {
+//             return ans ;
+//         }
+
+//         ans = BinarySearch(index,n-1, nums,target) ;
+//         return ans ;
+//     }
+// }
+
 class Solution {
 
-    public static int FindMin(int low , int high , int[] nums)
-    {
-        while(low < high)
-        {
-            int mid = low + (high-low)/2 ;
-            if(nums[mid] > nums[high])
-            {
-                low = mid + 1 ;
-            }
-            else
-            {
-                high = mid ;
+    public static int FindMin(int low, int high, int[] nums) {
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
-
-        return low ;
+        return low;
     }
 
-    public static int BinarySearch(int low , int high , int[] nums , int target)
-    {
-        while(low <= high)
-        {
-            int mid = low + (high-low)/2 ;
+    public static int BinarySearch(int low, int high, int[] nums, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if(nums[mid] == target)
-            {
-                return mid ;
-            }
-            else if(nums[mid] < target)
-            {
-                low = mid + 1 ;
-            }
-            else
-            {
-                high = mid-1 ;
-            }
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        return -1 ;
+        return -1;
     }
+
     public int search(int[] nums, int target) {
-        
-        int n = nums.length ;
 
-        int index = FindMin(0,n-1,nums) ;
-        int ans = BinarySearch(0,index-1,nums,target) ;
+        int n = nums.length;
 
-        if(ans != -1)
-        {
-            return ans ;
+        int index = FindMin(0, n - 1, nums);
+
+        // 🔥 optimization
+        if (nums[index] <= target && target <= nums[n - 1]) {
+            return BinarySearch(index, n - 1, nums, target);
+        } else {
+            return BinarySearch(0, index - 1, nums, target);
         }
-
-        ans = BinarySearch(index,n-1, nums,target) ;
-        return ans ;
     }
 }
